0cc3ac7fa2f4f34f9b03ea372b9449c82c79883b
Кирилл Архипов <arkhipovkirill2001@gmail.com>
2020-09-07 10:38:11 +0300

    Ball added

---
+++ src/ru/mirea/inbo0519/pr1/Ball.java
@@ -0,0 +1,51 @@
+package ru.mirea.inbo0519.pr1;
+
+public class Ball
+{
+    private String color;
+    private int radius;
+
+    public Ball(String color, int radius)
+    {
+        this.color = color;
+        this.radius = radius;
+    }
+
+    public Ball(String color)
+    {
+        this.color = color;
+        this.radius = 1;
+    }
+    public Ball()
+    {
+        this.color = "Pup";
+        this.radius = 1;
+    }
+    public void setRadius(int radius)
+    {
+        this.radius = radius;
+    }
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+    public int getRadius()
+    {
+        return radius;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+
+    @Override
+    public String toString()
+    {
+        return color + ", radius " + radius;
+    }
+
+    public float intoMeters()
+    {
+        return radius / 100f;
+    }
+}
---
+++ src/ru/mirea/inbo0519/pr1/TestBall.java
@@ -0,0 +1,16 @@
+package ru.mirea.inbo0519.pr1;
+
+public class TestBall
+{
+    public static void main(String[] args)
+    {
+        Ball b1 = new Ball("Green", 26);
+        Ball b2 = new Ball("Red", 41);
+        Ball b3 = new Ball("Cyan");
+        b3.setRadius(43);
+        System.out.println(b1);
+        System.out.println(b1.intoMeters());
+        System.out.println(b2.intoMeters());
+        System.out.println(b3.intoMeters());
+    }
+}
